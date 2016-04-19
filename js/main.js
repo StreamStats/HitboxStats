20d197559ebbad473ed1b7db81506bec22a87491
Bacon_Space <thestreamstats@gmail.com>
2016-04-19 09:39:25 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -21,7 +21,6 @@ function getData(){
          html += '<br><b><span class="label label-success">Followers:' + followers + '</b>';
          html += '<br><b><span class="label label-success">Broadcaster:' + Broadcaster + '</b>';
          html += "<br>";
-         html += '<br><b><span class="label label-success">Current Status: ' + online + '</b>';
          html += "<br>";
          html += '<br><b><span class="label label-info"><font color="White"><a href="https://hitbox.tv/' + Username + '">hitbox.tv/'+ Username +'</a></font></b>';
                   html += '<br><b><span class="label label-success">Joined on: '+joined+' </b>'.replace('T', ' at ');
