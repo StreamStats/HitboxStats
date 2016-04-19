361c142dd8486da57679bf80913e0674cb335974
Bacon_Space <thestreamstats@gmail.com>
2016-04-19 09:08:17 -0400

    Added files via upload

---
+++ js/main.js
@@ -0,0 +1,33 @@
+$(document).ready(function(event){
+   $('#submit').click(function(event){
+      getData();
+      
+   });
+   $('#formThing').submit(function(event){
+      event.preventDefault();
+      getData();
+   });
+});
+function getData(){
+    $.get('https://api.hitbox.tv/user/' + $('#inputText').val(), "", function(data){
+         
+         var Username = data['user_name'];
+         var followers = data['followers'];
+         var Broadcaster = data['user_is_broadcaster'];
+         var joined = data['live_since'];
+        var html = '<center><img src="' + avatar + '"width="100px" height="100px" style="border:3px solid red">';
+        html += '<h1><span class="label label-success">' + Username + '</h1>';
+         html += '<br><b><span class="label label-success">Followers:' + followers + '</b>';
+         html += '<br><b><span class="label label-success">Broadcaster:' + Broadcaster + '</b>';
+         html += "<br>";
+         html += '<br><b><span class="label label-success">Current Status: ' + online + '</b>';
+         html += "<br>";
+         html += '<br><b><span class="label label-info"><font color="White"><a href="https://hitbox.tv/' + Username + '">hitbox.tv/'+ Username +'</a></font></b>';
+                  html += '<br><b><span class="label label-success">Joined on: '+joined+' </b>'.replace('T', ' at ');
+         $('.profile').html(html);
+      }).fail(function(data){
+            html = '<h1>A hitbox user with that name does not exist.';
+             $('.profile').html(html);
+            return;
+      });
+}
